11671928c53357eb69c0cb7ef25cc4e4743bff66
Djain04 <dj7000@srmist.edu.in>
2026-02-02 19:45:31 +0530

    Add file-loader and enhanced webpack config for GLB assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    // Handle GLB/GLTF files as external resources
     config.module.rules.push({
       test: /\.(glb|gltf)$/,
       type: 'asset/resource',
+      generator: {
+        filename: 'static/[hash][ext]',
+      },
     });
+
+    // Disable caching for GLB files
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/files',
+            outputPath: 'static/files',
+            name: '[name].[ext]',
+            esModule: false,
+          },
+        },
+      ],
+    });
+
     return config;
+  },
+  // Ensure static files are served correctly
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
   },
 };
 
